33cd0736d4dfd81c31146071e26b0b1cbfad5998
Shahrukh Malik <shahrukhmlk@gmail.com>
2023-12-29 00:23:20 +0530

    fix errors

--- src/lib/data/directus.ts
+++
@@ -1,3 +0,0 @@
-import { createDirectus, rest } from "@directus/sdk"
-
-export const client = createDirectus(process.env.API_URL as string).with(rest())
--- src/lib/data/hadith/hadith.ts
+++ src/lib/data/hadith/hadith.ts
@@ -1,8 +1,3 @@
-import { readItems } from "@directus/sdk"
-import { format } from "date-fns"
-import { client } from "../directus"
-
 export async function getHadith(date: Date) {
-  const res = await client.request(readItems("hadiths", { fields: ["*.*.*"] }))
-  return res
+  return date.toISOString()
 }
