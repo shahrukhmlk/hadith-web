a5939c297a9171b68e296672630dbe3b31f28b5c
Shahrukh Malik <shahrukhmlk@gmail.com>
2023-12-29 00:20:31 +0530

    Update snapshot

--- src/components/hadith/hoc/HadithHoc.tsx
+++ src/components/hadith/hoc/HadithHoc.tsx
@@ -23,7 +23,7 @@ const HadithHoc = async ({ className, date, langs }: IHadithHoc) => {
     <div className={clsx("flex flex-col gap-4", className)}>
       <div className="flex items-baseline gap-4">
         <p className="w-40 text-left">111</p>
-        <h1 className="flex flex-1 justify-center">{hadith.length}</h1>
+        <h1 className="flex flex-1 justify-center">{JSON.stringify(hadith)}</h1>
         <p className="w-40 text-right">{selectedDate.toDateString()}</p>
       </div>
       <div className="flex flex-col justify-center gap-2 text-center">
