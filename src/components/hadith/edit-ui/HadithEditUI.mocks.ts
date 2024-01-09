e276f6e419c46ade4566c296fa9907b096f33aec
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-10 02:58:49 +0530

    Fix build errors and update prisma

--- src/components/hadith/edit-ui/HadithEditUI.mocks.ts
+++ src/components/hadith/edit-ui/HadithEditUI.mocks.ts
@@ -3,12 +3,9 @@ import { IHadithEditUI } from "./HadithEditUI"
 
 const base: IHadithEditUI = {
   className: undefined,
-  num: 5,
   lang: DEFAULT_LANGUAGE,
   topic: "Topic",
-  date: new Date(),
   text: "",
-  books: [{ name: "Book Name", hadithNum: 5 }],
 }
 
 export const mockHadithEditUiProps = {
