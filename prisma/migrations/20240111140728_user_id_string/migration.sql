fb7f29f25d920d0860437ccf403ce4b389237e8d
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-11 19:38:44 +0530

    userid string

---
+++ prisma/migrations/20240111140728_user_id_string/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "userCreated" SET DATA TYPE TEXT,
+ALTER COLUMN "userUpdated" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Hadith" ALTER COLUMN "userCreated" SET DATA TYPE TEXT,
+ALTER COLUMN "userUpdated" SET DATA TYPE TEXT;
