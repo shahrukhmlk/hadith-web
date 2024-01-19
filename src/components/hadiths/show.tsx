613fbf14555f47da84ec95cb54b61822ead88dfd
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-20 01:47:16 +0530

    Remove generated layouts

--- src/components/hadiths/show.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { AntdInferencer } from "@refinedev/inferencer/antd";
-
-export const HadithShow: React.FC<IResourceComponentsProps> = () => {
-    return <AntdInferencer />;
-};
