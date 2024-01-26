75f6e3137ffdf86887e043b2a8144e1453a4fb92
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-26 14:24:37 +0530

    Fix status badge props

--- src/components/badges/status/StatusBadge.mocks.ts
+++ src/components/badges/status/StatusBadge.mocks.ts
@@ -1,7 +1,8 @@
+import { Status } from "@/data/models/status/status"
 import { StatusBadgeProps } from "./StatusBadge"
 
 const base: StatusBadgeProps = {
-  className: undefined,
+  status: Status.draft,
 }
 
 export const mockStatusBadgeProps = {
--- src/components/badges/status/StatusBadge.tsx
+++ src/components/badges/status/StatusBadge.tsx
@@ -1,12 +1,16 @@
 import { Badge } from "@/components/ui/badge"
 import { Status } from "@/data/models/status/status"
 
-export interface StatusBadgeProps {
+export interface StatusBadgeProps extends React.HTMLAttributes<HTMLDivElement> {
   status: Status
 }
 
-const StatusBadge = ({ status }: StatusBadgeProps) => {
-  return <Badge variant="outline">{status}</Badge>
+const StatusBadge = ({ status, ...props }: StatusBadgeProps) => {
+  return (
+    <Badge variant="outline" {...props}>
+      {status}
+    </Badge>
+  )
 }
 
 export default StatusBadge
