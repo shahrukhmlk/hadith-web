c8b97af37ccd301034dabf110beded30ca723b50
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-28 18:51:44 +0530

    Add loading indicator

--- src/components/inputs/searchable-select/SearchableSelectInput.tsx
+++ src/components/inputs/searchable-select/SearchableSelectInput.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button"
 import {
   Command,
   CommandEmpty,
-  CommandInput,
   CommandItem,
   CommandList,
 } from "@/components/ui/command"
@@ -14,8 +13,15 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { cn } from "@/lib/utils"
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
+import {
+  CaretSortIcon,
+  CheckIcon,
+  MagnifyingGlassIcon,
+  ReloadIcon,
+} from "@radix-ui/react-icons"
 import { PopoverTriggerProps } from "@radix-ui/react-popover"
+import clsx from "clsx"
+import { CommandInput } from "cmdk"
 import { useState } from "react"
 
 export interface SelectItem {
@@ -67,13 +73,28 @@ export default function SearchableSelectInput({
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command shouldFilter={false} label={name}>
-          <CommandInput
-            placeholder={placeHolder}
-            onValueChange={onFilterChange}
-            className="h-9"
-          />
-          <CommandEmpty>{isLoading ? "Loading..." : emptyText}</CommandEmpty>
+          <div
+            className="flex items-center border-b px-3"
+            cmdk-input-wrapper=""
+          >
+            <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+            <CommandInput
+              placeholder={placeHolder}
+              onValueChange={onFilterChange}
+              name={name}
+              className={cn(
+                "flex h-10 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+              )}
+            />
+            <ReloadIcon
+              className={clsx(
+                "ml-2 h-4 w-4 shrink-0 animate-spin opacity-0",
+                isLoading && "opacity-50",
+              )}
+            />
+          </div>
           <CommandList>
+            <CommandEmpty>{emptyText}</CommandEmpty>
             {items.map((item) => (
               <CommandItem
                 value={item.value}
