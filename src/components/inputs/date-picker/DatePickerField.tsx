e204665da5db9c16bd38eedc57d8655f666e3b4c
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-29 20:44:16 +0530

    Add datepicker field

---
+++ src/components/inputs/date-picker/DatePickerField.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+import { CalendarIcon } from "@radix-ui/react-icons"
+import { format } from "date-fns"
+import { useState } from "react"
+
+export interface DatePickerFieldProps {
+  selected?: Date
+  onSelect?: (date: Date | undefined) => void
+}
+function DatePickerField({ selected, onSelect }: DatePickerFieldProps) {
+  const [datePickerOpen, setDatePickerOpen] = useState(false)
+
+  return (
+    <Popover open={datePickerOpen} onOpenChange={setDatePickerOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full pl-3 text-left font-normal",
+            !selected && "text-muted-foreground",
+          )}
+        >
+          {selected ? format(selected, "PPP") : <span>Pick a date</span>}
+          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          onDayClick={() => setDatePickerOpen(false)}
+          onSelect={onSelect}
+          selected={selected}
+          /* disabled={(date) =>
+              date > new Date() || date < new Date("1900-01-01")
+            } */
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default DatePickerField
