29f8c29786984836facba784a203b1b297d075e2
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-30 19:13:15 +0530

    improve zod schema for hadith book

--- src/data/models/hadith/hadith.ts
+++ src/data/models/hadith/hadith.ts
@@ -18,9 +18,9 @@ type Hadith = z.infer<typeof HadithSchema>
 export interface IHadith extends Hadith {}
 
 export const HadithBookSchema = z.object({
-  hadithID: z.coerce.number().int(),
-  bookID: z.coerce.number().int(),
-  hadithRefNumber: z.coerce.number().int(),
+  hadithID: z.coerce.number().int().min(1),
+  bookID: z.coerce.number().int().min(1),
+  hadithRefNumber: z.coerce.number().int().min(1),
 })
 type HadithBook = z.infer<typeof HadithBookSchema>
 export interface IHadithBook extends HadithBook {}
