7740db7b9790e7cb1f9ff08096537c0930c7f773
Shahrukh Malik <shahrukhmlk@gmail.com>
2024-01-30 19:12:56 +0530

    make arabic the default language, so its translations are not visible

--- src/data/language/getLanguages.ts
+++ src/data/language/getLanguages.ts
@@ -4,6 +4,9 @@ import prisma from "@/data/prisma"
 import { cache } from "react"
 
 export const getLanguages = cache(async (): Promise<ILanguage[]> => {
-  const res = await prisma.language.findMany({ orderBy: { sort: "asc" } })
+  const res = await prisma.language.findMany({
+    where: { code: { not: "ar" } },
+    orderBy: { sort: "asc" },
+  })
   return res
 })
